8d1c990b6b60cd90c51f1f2a0b819f7abfe6833e
landerAvrix <lander@avrix.es>
2026-05-14 22:33:16 +0200

    fix: remove BOM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { db, auth } from "./firebase";
 import {
   collection, doc, onSnapshot, setDoc, deleteDoc, updateDoc, addDoc
